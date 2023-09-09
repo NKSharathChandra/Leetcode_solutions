15e50bd4502b933fa51401d24df452dfe6295025
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2023-09-09 21:04:21 +0530

    377. Combination Sum IV 09-09-23.java solution

---
+++ September/09-09-23.java
@@ -0,0 +1,13 @@
+// Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+public int combinationSum4(int[] nums, int target) {
+    int[] comb = new int[target + 1];
+    comb[0] = 1;
+    for (int i = 1; i < comb.length; i++) {
+        for (int j = 0; j < nums.length; j++) {
+            if (i - nums[j] >= 0) {
+                comb[i] += comb[i - nums[j]];
+            }
+        }
+    }
+    return comb[target];
+}
