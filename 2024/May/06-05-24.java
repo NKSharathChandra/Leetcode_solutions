654492957a477e7dda1900119c730131ee8151a4
NKSharathChandra <128453485+NKSharathChandra@users.noreply.github.com>
2024-05-06 23:30:08 +0530

    2487. Remove Nodes From Linked List 06-05-24.java solution

---
+++ 2024/May/06-05-24.java
@@ -0,0 +1,21 @@
+ class Solution {   
+    public ListNode removeNodes(ListNode head) {
+        // Base case, reached end of the list
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Recursive call
+        ListNode nextNode = removeNodes(head.next);
+
+        // If the next node has greater value than head, delete the head
+        // Return next node, which removes the current head and makes next the new head
+        if (head.val < nextNode.val) {
+            return nextNode;
+        }
+
+        // Keep the head
+        head.next = nextNode;
+        return head;
+    }
+}
